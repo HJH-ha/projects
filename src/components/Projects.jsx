b649745b19e3e6a07070008147d3aeaa8e92d92c
JH <wlgud5632@naver.com>
2025-01-10 16:12:33 +0900

    first commit

---
+++ src/components/Projects.jsx
@@ -0,0 +1,26 @@
+import { projects } from "../data";
+
+const Projects = () => {
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => (
+          <a
+            href={project.url}
+            target="_blank"
+            rel="noreferrer"
+            className="project"
+          >
+            <img src={project.image} alt="birthday buddy" className="img" />
+            <h5>{project.title}</h5>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+};
+export default Projects;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+import p1 from "./assets/movie1.png";
+import p2 from "./assets/movie2.png";
+import p3 from "./assets/game.png";
+import p4 from "./assets/github.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Search Movie",
+    url: "https://fastidious-hamster-c949c0.netlify.app/",
+    image: p1,
+  },
+  {
+    id: 2,
+    title: "Movie List",
+    url: "https://search-movie-app-jhha.netlify.app/",
+    image: p2,
+  },
+  {
+    id: 3,
+    title: "Memory Game",
+    url: "https://magicmemorygame-jhha.netlify.app/",
+    image: p3,
+  },
+  {
+    id: 4,
+    title: "github-finder",
+    url: "https://githubfinder-jhha.netlify.app/",
+    image: p4,
+  },
+  {
+    id: 4,
+    title: "github-finder",
+    url: "https://githubfinder-jhha.netlify.app/",
+    image: p4,
+  },
+  {
+    id: 4,
+    title: "github-finder",
+    url: "https://githubfinder-jhha.netlify.app/",
+    image: p4,
+  },
+  {
+    id: 4,
+    title: "github-finder",
+    url: "https://githubfinder-jhha.netlify.app/",
+    image: p4,
+  },
+  {
+    id: 4,
+    title: "github-finder",
+    url: "https://githubfinder-jhha.netlify.app/",
+    image: p4,
+  },
+];
